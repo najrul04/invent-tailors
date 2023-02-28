a9e16e4d98d32c6165690bd517773c8daeadfabf
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2023-02-28 20:52:16 +0600

    Product Picture

--- src/Pages/Home/Team/TeamMember.js
+++ src/Pages/Home/Team/TeamMember.js
@@ -8,9 +8,9 @@ const TeamMember1 = ({team}) => {
        <div className="bg-base-100 shadow-xl border border-inherit p-6 rounded-lg w-full">
        <img className="swiper_image rounded-lg" src={img} alt="" />
        <div className="pt-6 text-center">
-           <h1 className="mt-2 font-bold">{name}</h1>
-           <h3 className="my-2">{title}</h3>
-           <span>Experience: {experience}</span>
+           <h1 className="mt-2 font-bold text-lg">{name}</h1>
+           <h3 className="my-2 font-semibold">{title}</h3>
+           <span className='text-sm'>Experience: {experience}</span>
        </div>
    </div>
     );
